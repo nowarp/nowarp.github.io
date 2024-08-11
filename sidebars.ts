7acebccf5c4c47adaff588e86363b82e6ae0e428
Georgiy Komarov <jubnzv@gmail.com>
2024-08-11 01:40:44 +0000

    feat(docs): Add CHANGELOG

--- sidebars.ts
+++ sidebars.ts
@@ -83,6 +83,11 @@ const sidebars: SidebarsConfig = {
           id: 'hacking/custom-detector',
           label: 'Custom Detectors',
         },
+        {
+          type: 'doc',
+          id: 'hacking/CHANGELOG',
+          label: 'CHANGELOG',
+        },
       ],
     },
   ],
