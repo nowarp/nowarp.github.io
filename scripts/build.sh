d1ad213bdc0f4cab0d126627f9f5a10aa53708c3
Georgiy Komarov <jubnzv@gmail.com>
2024-11-12 02:18:48 +0000

    feat(scripts): Custom paths to misti/souffle.js

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+# Use paths from environment variables if set
+MISTI_PATH=${MISTI_PATH:-../misti}
+SOUFFLE_PATH=${SOUFFLE_PATH:-../souffle.js}
+CUSTOM_CSS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../src/css/typedoc-custom.css"
+
 echo 'Generating Misti TypeDoc documentation...'
-pushd ../misti
-yarn docs --customCss ../nowarp.github.io/src/css/typedoc-custom.css
+pushd "$MISTI_PATH"
+yarn docs --customCss "$CUSTOM_CSS_PATH"
 popd > /dev/null
 rm -rf static/tools/misti/api
 mkdir -pv static/tools/misti
-mv ../misti/docs/api static/tools/misti/
+mv "$MISTI_PATH/docs/api" static/tools/misti/
 
 echo 'Generating Souffle.js TypeDoc documentation...'
-pushd ../souffle.js
-yarn docs --customCss ../nowarp.github.io/src/css/typedoc-custom.css
+pushd "$SOUFFLE_PATH"
+yarn docs --customCss "$CUSTOM_CSS_PATH"
 popd > /dev/null
 rm -rf static/lib/souffle-js/api
 mkdir -pv static/lib/souffle-js
-mv ../souffle.js/docs/api static/lib/souffle-js/
+mv "$SOUFFLE_PATH/docs/api" static/lib/souffle-js/
 
 echo 'Generating docusaurus documentation...'
 rm -rf ./build
 docusaurus build --out-dir build/
-
