d52035f6a1f4d0714dfc2848bdecd36d7243b121
Georgiy Komarov <jubnzv@gmail.com>
2026-05-01 14:55:39 +0700

    feat(blog): Add RSS icon near `Latest posts`

---
+++ src/theme/BlogSidebar/Desktop/index.tsx
@@ -0,0 +1,56 @@
+import React, {memo} from 'react';
+import clsx from 'clsx';
+import {translate} from '@docusaurus/Translate';
+import {
+  useVisibleBlogSidebarItems,
+  BlogSidebarItemList,
+} from '@docusaurus/plugin-content-blog/client';
+import BlogSidebarContent from '@theme/BlogSidebar/Content';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faRss} from '@fortawesome/free-solid-svg-icons';
+import styles from './styles.module.css';
+
+const ListComponent = ({items}) => {
+  return (
+    <BlogSidebarItemList
+      items={items}
+      ulClassName={clsx(styles.sidebarItemList, 'clean-list')}
+      liClassName={styles.sidebarItem}
+      linkClassName={styles.sidebarItemLink}
+      linkActiveClassName={styles.sidebarItemLinkActive}
+    />
+  );
+};
+
+function BlogSidebarDesktop({sidebar}) {
+  const items = useVisibleBlogSidebarItems(sidebar.items);
+  return (
+    <aside className="col col--3">
+      <nav
+        className={clsx(styles.sidebar, 'thin-scrollbar')}
+        aria-label={translate({
+          id: 'theme.blog.sidebar.navAriaLabel',
+          message: 'Blog recent posts navigation',
+          description: 'The ARIA label for recent posts in the blog sidebar',
+        })}>
+        <div className={clsx(styles.sidebarItemTitle, styles.titleRow, 'margin-bottom--md')}>
+          <span>{sidebar.title}</span>
+          <a
+            href="/blog/rss.xml"
+            className={styles.rssLink}
+            title="Subscribe via RSS"
+            aria-label="Subscribe via RSS">
+            <FontAwesomeIcon icon={faRss} />
+          </a>
+        </div>
+        <BlogSidebarContent
+          items={items}
+          ListComponent={ListComponent}
+          yearGroupHeadingClassName={styles.yearGroupHeading}
+        />
+      </nav>
+    </aside>
+  );
+}
+
+export default memo(BlogSidebarDesktop);
